5e3f3a61ccfa324580d07b6fd71306d3c360e555
ydh8908 <58350120+ydh8908@users.noreply.github.com>
2019-12-09 00:26:54 +0900

    Upbit_매도 구현

--- Upbity_Buy.py
+++ Upbity_Buy.py
@@ -24,7 +24,7 @@ while True:
     hold_list = []
     for i in hold_balance:
         hold_list.append("KRW-"+i['currency'])
-    buy_target = set(tickers) - set(hold_list)
+    buy_target = set(tickers) - set(hold_list) #매수주문이 대기상태인 코인들을 안빼고있음
     for tick in buy_target:
         target = get_target_price(tick)
         current_price = pyupbit.get_current_price(tick)
@@ -32,23 +32,48 @@ while True:
         today_price = price_info.iloc[-1]
         today_high = today_price['high']
         today_open = today_price['open']
-        today_max= (today_high-today_open)/today_open
-        # yesterday_info= price_info.iloc[-2]
-        # yesterday_volume = yesterday_info['volume']*yesterday_info['close']
-        if current_price>=target and today_max<0.1:
+        today_max = (today_high-today_open)/today_open
+        yes = pyupbit.get_daily_ohlcv_from_base(tick).iloc[-2]
+        yes_vol = yes['close']*yes['volume']
+        time.sleep(0.1)
+        if current_price>=target and today_max<0.1 and yes_vol>150000000:
             # yesterday volume 100분의 1 에 한해 가능한 밸런스를 오더넣는거 구현 필요
             #
             krw = 1000
             orderbook = pyupbit.get_orderbook(tick)
-            seller_price = orderbook[0]['orderbook_units'][3]['ask_price']
-            unit = format((krw / seller_price),'.2f') #sell price dynamic하게 구현필요. 왜 오류나는지 모름
-            print(upbit.buy_limit_order(tick,seller_price,unit))
-            time.sleep(0.5)
-            sell_price = seller_price*1.3+0.1
-            print(upbit.sell_limit_order(tick, sell_price,unit))
-        time.sleep(0.1)
-        #아예 지정가 매수주문으로 변경시키고, 매도를 루프로 구혀내서 매수가격 * 1.3으로 구ㅕㅎㄴ
-        #그 후 특정 시간 e.g. 11:48에 전량 매도
+            price = orderbook[0]['orderbook_units'][0]['bid_price']
+            unit = format((krw / price),'.2f')
+            print(tick)
+            print(upbit.buy_limit_order(tick,price,unit))
+
+        hold_balance = upbit.get_balances()[0]
+        for i in hold_balance[1:]:
+            sell_price = float(i['avg_buy_price']) * 1.28
+            if sell_price < 10:
+                adj_sell_price = format(sell_price, '.2f')
+            elif 10 <= sell_price < 100:
+                adj_sell_price = format(sell_price, '.1f')
+            elif 100 <= sell_price < 1000:
+                adj_sell_price = round(sell_price)
+            elif 1000 <= sell_price < 10000:
+                adj_sell_price = sell_price - (sell_price % 5)
+            elif 10000 <= sell_price < 100000:
+                adj_sell_price = sell_price - (sell_price % 10)
+            elif 100000 <= sell_price < 500000:
+                adj_sell_price = sell_price - (sell_price % 50)
+            elif 500000 <= sell_price < 1000000:
+                adj_sell_price = sell_price - (sell_price % 100)
+            elif 1000000 <= sell_price < 2000000:
+                adj_sell_price = sell_price - (sell_price % 500)
+            elif 2000000 <= sell_price:
+                adj_sell_price = sell_price - (sell_price % 1000)
+
+            if float(i['balance']) < 0.01:
+                continue
+            else:
+                print(upbit.sell_limit_order("KRW-" + i['currency'], adj_sell_price, float(i['balance'])))
+
+        #그 특정 시간 e.g. 11:48에 보유주문 전량 취소 후전량 매도
 
 
 
